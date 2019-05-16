908a98c70c49bd8f43be97e63ca5f7cd52b3c4ea
Heverton13 <heverton.gomes2018@gmail.com>
2019-05-16 10:49:51 -0300

    Implementações de novas views

--- inventario/courses/admin.py
+++ inventario/courses/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Course
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'start_date', 'create_at']
+    search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Course, CourseAdmin)
 
 
